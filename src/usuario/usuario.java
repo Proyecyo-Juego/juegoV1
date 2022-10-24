ddd1a35128bffc8bb23e108e3c774b7db93c2ce3
vnavacar <98817213+vnavacar@users.noreply.github.com>
2022-10-24 09:47:32 +0200

    usuario

---
+++ src/usuario/usuario.java
@@ -0,0 +1,17 @@
+package usuario;
+
+public class usuario{
+
+    String nombre;
+    String email;
+    String password;
+
+    public usuario(String nombre, String email, String password) {
+        this.nombre = nombre;
+        this.email = email;
+        this.password = password;
+    }
+
+
+
+}
