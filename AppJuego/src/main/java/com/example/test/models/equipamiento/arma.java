1ec81b9052c12db85fe3948cf95ef7878661d7db
David César Moro Nieto <davidmoro96@gmail.com>
2022-11-02 00:10:00 +0100

    cambios

---
+++ AppJuego/src/main/java/com/example/test/models/equipamiento/arma.java
@@ -0,0 +1,10 @@
+package com.example.test.models.equipamiento;
+
+
+public class arma extends equipamiento{
+    //extiende equipamiento, contiene atributos de stats y posiblemente habilidades?
+
+    public arma(int valor, String nombre, String descripcion) {
+        super(valor, nombre, descripcion);
+    }
+}
