21cca665f0420d39eb271ead2131798b853fd651
vnavacar <vnavacar@myuax.com>
2022-12-29 18:21:53 +0100

    UI empezada

--- AppJuego/src/main/java/com/example/test/TestApplication.java
+++ AppJuego/src/main/java/com/example/test/TestApplication.java
@@ -1,5 +1,6 @@
 package com.example.test;
 
+import com.example.test.models.ui.UIMain;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestApplication {
 
     public static void main(String[] args) {
+        new UIMain();
         SpringApplication.run(TestApplication.class, args);
     }
 
---
+++ AppJuego/src/main/java/com/example/test/models/ui/UIMain.java
@@ -0,0 +1,8 @@
+package com.example.test.models.ui;
+
+import javax.swing.*;
+
+public class UIMain {
+
+    private JButton button1;
+}
